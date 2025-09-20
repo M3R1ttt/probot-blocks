e686055a9ca526fe7952f0dac2d194f75cf1e387
tunapro <tunagul54@gmail.com>
2025-09-21 01:11:40 +0300

    feat: expand text toolbox options

--- src/App.tsx
+++ src/App.tsx
@@ -305,6 +305,18 @@ function App() {
           contents: [
             { kind: 'block', type: 'text' },
             { kind: 'block', type: 'text_join' },
+            {
+              kind: 'block',
+              type: 'text_join',
+              extraState: {
+                itemCount: 3,
+              },
+            },
+            { kind: 'block', type: 'text_length' },
+            { kind: 'block', type: 'text_isEmpty' },
+            { kind: 'block', type: 'text_changeCase' },
+            { kind: 'block', type: 'text_trim' },
+            { kind: 'block', type: 'text_print' },
           ],
         },
         {
